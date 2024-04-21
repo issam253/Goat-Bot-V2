d783405b7ff10218453f39394c760cc43bb81ba1
issam253 <166600559+issam253@users.noreply.github.com>
2024-04-21 18:28:26 +0000

    Update cmds

---
+++ scripts/cmds/uptime.js
@@ -0,0 +1,57 @@
+module.exports = {
+  config: {
+    name: "ابتايم",
+    aliases: ["uptime"],
+    role: 0,
+    shortDescription: {
+      en: "ابتايم سرفر",
+      tl: "Ipakita ang uptime ng server",
+    },
+    longDescription: {
+      en: "مدة تشغيل سرفر البوت",
+      tl: "Ipapakita ang tagal na gumagana ang server",
+    },
+    category: "النظام",
+    guide: {
+      en: "{p}ابتايم",
+      tl: "{p}uptime",
+    },
+  },
+
+  onStart: async function ({ api, message }) {
+    const os = require("os");
+    const uptime = os.uptime();
+
+    const days = Math.floor(uptime / (3600 * 24));
+    const hours = Math.floor((uptime % (3600 * 24)) / 3600);
+    const mins = Math.floor((uptime % 3600) / 60);
+    const seconds = Math.floor(uptime % 60);
+
+    const system = `𝗢𝘀: ${os.platform()} ${os.release()}`;
+    const cores = `𝗖𝗼𝗿𝗲𝘀: ${os.cpus().length}`;
+    const arch = `𝗔𝗿𝗰𝗵𝗶𝘁𝗲𝗰𝘁𝘂𝗿𝗲: ${os.arch()}`;
+    const totalMemory = `𝗧𝗼𝘁𝗮𝗹 𝗠𝗲𝗺𝗼𝗿𝘆: ${Math.round(os.totalmem() / (1024 * 1024 * 1024))} GB`;
+    const freeMemory = `𝗙𝗿𝗲𝗲 𝗠𝗲𝗺𝗼𝗿𝘆: ${Math.round(os.freemem() / (1024 * 1024 * 1024))} GB`;
+    const uptimeString = `𝘂𝗽𝘁𝗶𝗺𝗲: ${days} 𝗱𝗮𝘆𝘀, ${hours} 𝗵𝗼𝘂𝗿𝘀, ${mins} 𝗺𝗶𝗻𝘂𝘁𝗲𝘀, 𝗮𝗻𝗱  ${seconds} 𝘀𝗲𝗰𝗼𝗻𝗱𝘀`;
+
+    const response = `🕒 ${uptimeString}\n━━━━━━━━━━━━━\n\n📡 ${system}\n🛡 ${cores}\n⚔ البوت نشط🟢\n📊 المستخدم من الرام: ${Math.round(process.memoryUsage().rss / (1024 * 1024))} MB\n💽 مجموع الرام: ${Math.round(os.totalmem() / (1024 * 1024 * 1024))} GB\n💾 رام المستخدم: ${Math.round(os.freemem() / (1024 * 1024 * 1024))} GB\n⏰ وقت التشغيل: ${Math.floor(process.uptime())}\n━━━━━━━━━━━━━`;
+
+    const gifAttachment = await global.utils.getStreamFromURL("https://i.imgur.com/PzkRrlw.gif");
+
+    message.reply({
+      body: response,
+      attachment: gifAttachment
+    });
+  },
+
+  onChat: async function({ event, message, getLang }) {
+    if (event.body && event.body.toLowerCase() === "uptime") {
+      const gifAttachment = await global.utils.getStreamFromURL("https://i.imgur.com/PzkRrlw.gif");
+
+      return message.reply({
+        body: "",
+        attachment: gifAttachment
+      });
+    }
+  }
+};
